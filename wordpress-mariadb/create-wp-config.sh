1104a91b1a413bf2970997e1332ef85bc5d00584
Vu Hung <153976259+vuhung16au@users.noreply.github.com>
2025-05-18 15:48:05 +1000

    minor php handler fix

---
+++ wordpress-mariadb/create-wp-config.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cat > /var/www/html/wp-config.php << EOL
+<?php
+define("DB_HOST", getenv("WORDPRESS_DB_HOST"));
+define("DB_USER", getenv("WORDPRESS_DB_USER"));
+define("DB_PASSWORD", getenv("WORDPRESS_DB_PASSWORD"));
+define("DB_NAME", getenv("WORDPRESS_DB_NAME"));
+define("AUTH_KEY", "put your unique phrase here");
+define("SECURE_AUTH_KEY", "put your unique phrase here");
+define("LOGGED_IN_KEY", "put your unique phrase here");
+define("NONCE_KEY", "put your unique phrase here");
+define("AUTH_SALT", "put your unique phrase here");
+define("SECURE_AUTH_SALT", "put your unique phrase here");
+define("LOGGED_IN_SALT", "put your unique phrase here");
+define("NONCE_SALT", "put your unique phrase here");
+\$table_prefix = "wp_";
+define("WP_DEBUG", false);
+if ( !defined("ABSPATH") ) define("ABSPATH", dirname(__FILE__) . "/");
+require_once(ABSPATH . "wp-settings.php");
+EOL
